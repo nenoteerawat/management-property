13e83023b8726d10e0922b315c62052bbaf3aab9
teerawat.yamsai <teerawat.yamsai@krungsri.com>
2020-07-19 10:15:55 +0700

    add template management

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,7 @@
-import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import { shallowMount } from '@vue/test-utils'
+import App from '@/App.vue'
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    });
-    expect(wrapper.text()).toMatch(msg);
-  });
-});
+test('App should work', () => {
+  const wrapper = shallowMount(App)
+  expect(wrapper.text()).toMatch('Welcome to Your Vue.js App')
+})
